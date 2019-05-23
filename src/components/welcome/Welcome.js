bb3eb49dac11cb4902f35c1a1bbd2748bb3ea86e
sarziv <saruzivila12@gmail.com>
2019-05-23 17:42:26 +0300

    welcome change

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -2,19 +2,27 @@ import React from 'react';
 import './Welcome.css';
 
 function Welcome() {
-        return (
-            <div>
-                <section>
-                    <div className="wave">
-                        <div className="bls"><b>BLS</b>
-                        <div><b>S</b>alary</div>
-                        <div><b>S</b>ystem</div>
-                        </div>
-                        <div className="joinButton"><button className="btn btn-outline-light">Prisijunk!</button></div>
+    return (
+        <div>
+            <section>
+                <div className="wave">
+                    <div className="bls">
+                        <b>BLS</b>
                     </div>
-                </section>
-            </div>
-        );
+                    <div className="bls">
+                        <b>S</b>alary
+                    </div>
+                    <div className="bls">
+                        <b>S</b>ystem
+                    </div>
+                    <div className="bls"><h5>Prisijunk prie komandos!</h5></div>
+                    <div className="joinButton">
+                        <button className="btn btn-outline-light">Prisijungti</button>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default Welcome;
