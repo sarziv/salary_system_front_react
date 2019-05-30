89dedec86abcd987e4127448f8005dbe3249f0b3
sarziv <saruzivila12@gmail.com>
2019-05-31 02:46:24 +0300

    mapping fixed

--- src/components/user/records/Records.js
+++ src/components/user/records/Records.js
@@ -7,16 +7,19 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
+
 function Records() {
 
+
     const userData = [
-        {id:1,date:5-28,pallet:250,lines:250,VIP:1,hour:1},
-        {id:2,date:5-27,pallet:251,lines:251,VIP:1,hour:1},
-        {id:3,date:5-21,pallet:252,lines:252,VIP:1,hour:1}
+        {id:1,date:'5-25',pallet:250,lines:250,VIP:1,hour:1},
+        {id:2,date:'5-24',pallet:251,lines:251,VIP:1,hour:1},
+        {id:3,date:'5-23',pallet:252,lines:252,VIP:1,hour:1}
     ];
 
     function DataDisplay(props) {
-        const listDisplay = userData.map((record) =>
+
+        const listDisplay = props.userData.map((record) =>
 
             <ExpansionPanel>
                 <ExpansionPanelSummary
@@ -27,8 +30,10 @@ function Records() {
                     <Typography>
                         <div className="d-flex justify-content-between">
                             <div className="float-left">{record.date}</div>
-                            <div className="pl-5">$</div>
-
+                            <div className="pl-4">
+                                {record.pallet * 0.11 + record.lines * 0.09 + record.VIP * 3}€
+                                + { record.hour !== 0 ? record.hour * 7 +'€': ''}
+                            </div>
                         </div>
                     </Typography>
                 </ExpansionPanelSummary>
@@ -46,17 +51,17 @@ function Records() {
             );
 
         return (
-            {listDisplay}
+            listDisplay
         )}
 
     return (
         <div className="container-fluid toppading">
             <h4 className="d-flex justify-content-center">Istorija</h4>
             <h6 className="d-flex">
-                <div className="pl-3">Laikas</div>
+                <div className="pl-3">Data</div>
                 <div className="pl-4">Uždirbta</div>
             </h6>
-           <DataDisplay props={userData}/>
+           <DataDisplay userData={userData}/>
         </div>
     );
 }
