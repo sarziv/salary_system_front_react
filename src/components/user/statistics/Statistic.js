d3ed645933108a0d6a0e2a92901f3ab3929afc05
sarziv <saruzivila12@gmail.com>
2019-06-06 03:57:22 +0300

    Statistic update

--- src/components/user/statistics/Statistic.js
+++ src/components/user/statistics/Statistic.js
@@ -1,14 +1,75 @@
 import React from 'react';
 import './Statistic.css';
 
+
+function getMonthByName() {
+    const date = new Date().getMonth() + 1;
+
+    switch (date) {
+        case 1:
+            return 'Sausis';
+        case 2:
+            return 'Vasaris';
+        case 3:
+            return 'Kovas';
+        case 4:
+            return 'Balandis';
+        case 5:
+            return 'Gegužė';
+        case 6:
+            return 'Birželis';
+        case 7:
+            return 'Liepa';
+        case 8:
+            return 'Rugpjūtis';
+        case 9:
+            return 'Rugsėjis';
+        case 10:
+            return 'Spalis';
+        case 11:
+            return 'Lapkritis';
+        case 12:
+            return 'Gruodis';
+        default:
+            return null;
+    }
+}
+
+const salary  = 1000;
+const hours  = 124;
 function Statistic() {
+
+
     return (
         <div className="toppading">
             <h4 className="d-flex justify-content-center">
-                Informacija
+                Statistika
             </h4>
-            <div>
-                
+            <div className="container">
+                <div>
+                    Mėnesis:
+                    <span className="text-white pl-2">
+                        {getMonthByName()}
+                    </span>
+                </div>
+                <div>
+                    Uždirbta:
+                    <span className="text-white pl-2">
+                        {salary} €
+                    </span>
+                </div>
+                <div>
+                    Valandos:
+                    <span className="text-white pl-2">
+                        {hours}
+                    </span>
+                </div>
+                <div>
+                    Valandinis:
+                    <span className="text-white pl-2">
+                        {(salary/hours).toFixed(1)+' '}€/h
+                    </span>
+                </div>
             </div>
         </div>
     );
