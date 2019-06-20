d91eea313f70b8c3dacab138c53a9c3aaaca94eb
sarziv <saruzivila12@gmail.com>
2019-06-20 23:25:22 +0300

    APi routes for records

--- src/components/router/Api.js
+++ src/components/router/Api.js
@@ -7,3 +7,8 @@ export const SIGNUP = URL+'/auth/signup';
 export const LOGOUT = URL+'/auth/logout';
 export const LOGIN = URL+'/auth/login';
 export const USER = URL+'/auth/user';
+//USER
+export const RECORDS = URL+'/user/records';
+export const ADD = URL+'/user/add';
+export const STATISTIC = URL+'/user/statistic';
+export const SEARCH = URL+'/user/search';
