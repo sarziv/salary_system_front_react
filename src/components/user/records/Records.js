6cc5c006664d18baf3a9885514b7959717e08ca2
sarziv <saruzivila12@gmail.com>
2019-06-21 00:14:09 +0300

    comments

--- src/components/user/records/Records.js
+++ src/components/user/records/Records.js
@@ -21,6 +21,7 @@ function Records() {
         async function fetchData() {
             await axios.get(API.RECORDS, {
                 params: {},
+                //TODO REMOVE change to access_token
                 headers: {'Authorization': 'Bearer ' + "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImJiMDYzZTU1ZWI0YWQ0NTNlNWU1ZGE3MmNkMTY2MzIyNzAyY2EwMWFmZmIzMDA0NTBkNmNiYWRlNDhkNzkwODE2OTZhMmQwYWY0NWFkMDAzIn0.eyJhdWQiOiIxIiwianRpIjoiYmIwNjNlNTVlYjRhZDQ1M2U1ZTVkYTcyY2QxNjYzMjI3MDJjYTAxYWZmYjMwMDQ1MGQ2Y2JhZGU0OGQ3OTA4MTY5NmEyZDBhZjQ1YWQwMDMiLCJpYXQiOjE1NjEwNjA2NTksIm5iZiI6MTU2MTA2MDY1OSwiZXhwIjoxNTkyNjgzMDU5LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.qrGBvhg3qjYIoc9N6-eS12e53iShvn0ix0dwQQSYRAXPFsKKh27A_bstBCiJ650AxncBkKXYTEYkKUZpur-Q4XAc9v03w8TRhqDbhK_DtWHVJ0Gw4cq5sm6Dx4tfTf08oFpbYA5daIx_CPmxGU2LQ3N6kEZBBIjCpPaWFpQGHnX2q_fPt4eWGSt93QhorPLMsuNbhtrLSgwQGQ1bqkxpljCyH7BLkn1LWZZWp0YrTeQPlZFiALTBDmcOyaGOuiUbSAujeuGX5s_5-YPPU56VFqDc4VvYeWpJpDs-V-zMk1cN84ku2tXBju0dqkoRvjBYjKeLe_orTa66VaLBw29oFI7lQsZby2CgptLaa8KLtqq7ZUcS7mqfgKS9goDbPDeR_VYG-aNexPToz3fNc0IH4qA7gyWujXXhRccWtq7ti0AF9-NSJ6r2stof8WogZPXH7YF0maTujTZSv-kHOshi-h22V8IhlN0kcbZMgYfndEf13tvzaoHQ0aQASO7YkoRP2I45xsPNF-hXJYwhT64aBXuGPl9_UBGsODKjRgeixTLmECgHLxWGnJJjb8PGpyCDYW1gwbJyiXOtIPCwMzyiybxvk_nnjarFFBIhzoTs8y2hqGBiqEFdyGu8J1nJe2ATkgOUq3S8OXz18-kGwJqAKjbEUIom2MPjlFt6jJvbIvE"}
             })
                 .then(function (response) {
@@ -37,59 +38,60 @@ function Records() {
     function DataDisplay() {
 
 
-        function CountMoney(lines,pallet,vip,extraHour) {
-            return (lines * 0.09 + pallet * 0.11+ vip *3+extraHour*6).toFixed(1);
+        function CountMoney(lines, pallet, vip, extraHour) {
+            return (lines * 0.09 + pallet * 0.11 + vip * 3 + extraHour * 6).toFixed(1);
         }
 
+        //TODO Format to YYYY-MM-DD
         function formatDate(date) {
             return date;
         }
 
         const listDisplay = records.data.map((record) =>
 
-                <ExpansionPanel>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header">
-                        <Typography component={'span'}>
-                            <div className="d-flex justify-content-between">
-                                <div className="float-left">{formatDate(record.created_at)}</div>
-                                <div className="pl-4">
-                                    {CountMoney(record.lines,record.pallet,record.vip,record.extra_hour)}
-                                </div>
+            <ExpansionPanel>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header">
+                    <Typography component={'span'}>
+                        <div className="d-flex justify-content-between">
+                            <div className="float-left">{formatDate(record.created_at)}</div>
+                            <div className="pl-4">
+                                {CountMoney(record.lines, record.pallet, record.vip, record.extra_hour) + '€'}
                             </div>
+                        </div>
+                    </Typography>
+                </ExpansionPanelSummary>
+                <div className="col-12">
+                    <ExpansionPanelDetails className="d-inline">
+                        <Typography component={'span'}>
+                            <ul className="list-group list-group-flush border-dark">
+                                <li className="list-group-item">Paletės:
+                                    <span className="badge badge-primary badge-pill float-right">{record.pallet}</span>
+                                </li>
+                                <li className="list-group-item">Eilutės:
+                                    <span className="badge badge-primary badge-pill float-right">{record.lines}</span>
+                                </li>
+                                <li className="list-group-item">VIP:
+                                    <span className="badge badge-primary badge-pill float-right">{record.vip}</span>
+                                </li>
+                                <li className="list-group-item">Valandos:
+                                    <span
+                                        className="badge badge-primary badge-pill float-right">{record.extra_hour}</span>
+                                </li>
+                            </ul>
                         </Typography>
-                    </ExpansionPanelSummary>
-                    <div className="col-12">
-                        <ExpansionPanelDetails className="d-inline">
-                            <Typography component={'span'}>
-                                <ul className="list-group list-group-flush border-dark">
-                                    <li className="list-group-item">Paletės:
-                                        <span className="badge badge-primary badge-pill float-right">{record.pallet}</span>
-                                    </li>
-                                    <li className="list-group-item">Eilutės:
-                                        <span className="badge badge-primary badge-pill float-right">{record.lines}</span>
-                                    </li>
-                                    <li className="list-group-item">VIP:
-                                        <span className="badge badge-primary badge-pill float-right">{record.vip}</span>
-                                    </li>
-                                    <li className="list-group-item">Valandos:
-                                        <span
-                                            className="badge badge-primary badge-pill float-right">{record.extra_hour}</span>
-                                    </li>
-                                </ul>
-                            </Typography>
-                        </ExpansionPanelDetails>
-                    </div>
-                </ExpansionPanel>
+                    </ExpansionPanelDetails>
+                </div>
+            </ExpansionPanel>
         );
         return (
             listDisplay
         )
     }
 
-
+    //TODO Remove after
     return /*auth === true ? */(
         <div className="container-fluid toppading">
             <h4 className="d-flex justify-content-center">Istorija</h4>
