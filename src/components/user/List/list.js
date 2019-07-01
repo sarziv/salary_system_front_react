5cfaed2801045f4bd9ce3a4dad24930a2a783b65
sarziv <saruzivila12@gmail.com>
2019-07-01 04:43:01 +0300

    travis added

--- src/components/user/List/list.js
+++ src/components/user/List/list.js
@@ -3,7 +3,7 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from "@material-ui/core/Typography";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import moment from "moment";
 import {useSelector} from "react-redux";
 import axios from "axios";
